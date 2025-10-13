cb39e36cb1c08e297e1af6073a103a741eb3064a
Cyriac <drevia59@gmail.com>
2025-10-13 11:31:23 +0200

    create new object

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -9,10 +9,18 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<any> = of(null);
+  public italyParticipationsCount$: Observable<number> = of(0);
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+      this.italyParticipationsCount$ = this.olympics$.pipe(
+      map((data) => {
+        if (!data) return 0; // Sécurité
+        const italy = data.find((o: { country: string; }) => o.country === 'Italy');
+        return italy ? italy.participations.length : 0;
+      })
+    );
   }
 }
